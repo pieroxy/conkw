104687a0f68e076d111d0e20232d27bdf691efef
Pierre Grimaud <pieroxy@pieroxy.net>
2021-02-20 09:28:05 +0100

    ✨ Server starts with 50m default heap size.

--- src/java/src/main/java/net/pieroxy/conkw/standalone/Installer.java
+++ src/java/src/main/java/net/pieroxy/conkw/standalone/Installer.java
@@ -55,7 +55,7 @@ public class Installer {
                 new File(ConfigReader.getBinDir(), "conkw.jar").toPath(),
                 StandardCopyOption.REPLACE_EXISTING);
         FileOutputStream script = new FileOutputStream(new File(ConfigReader.getBinDir(), "run.sh"));
-        script.write((java + "/bin/java -jar " + ConfigReader.getBinDir() + File.separator + "conkw.jar --run-server >> " + ConfigReader.getLogDir() + "/system.log 2>&1\n").getBytes());
+        script.write((java + "/bin/java -Xms50m -jar " + ConfigReader.getBinDir() + File.separator + "conkw.jar --run-server >> " + ConfigReader.getLogDir() + "/system.log 2>&1\n").getBytes());
         script.close();
     }
 
