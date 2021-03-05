1ad1adba3695f6cf6850b9c5736bedb1f91983e3
Pierre Grimaud <pieroxy@pieroxy.net>
2021-03-05 09:20:27 +0100

    ✨ OSHI: Improved processes UI reference to look a bit like top.
    Also, some columns show a + indicating there is details in the tooltip.

--- src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/oshi/OshiGrabber.java
+++ src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/oshi/OshiGrabber.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.time.Duration;
 import java.util.*;
-import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class OshiGrabber extends AsyncGrabber {
