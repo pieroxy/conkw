a6781099f9b840e48a8a5d02de7cf1f856f88f1a
Pierre Grimaud <pieroxy@pieroxy.net>
2021-04-03 07:42:52 +0200

    ✨ Exposes the .conkw/ui directory to prepare for customized UIs.

--- src/java/src/main/java/net/pieroxy/conkw/standalone/Runner.java
+++ src/java/src/main/java/net/pieroxy/conkw/standalone/Runner.java
@@ -18,49 +18,68 @@ public class Runner {
     public static void main(String[] args) throws Exception {
         if (has(args, "--run-server")) {
             Config config = ConfigReader.getConfig();
-            File webappDirLocation = ConfigReader.getWebappDir();
-            Tomcat tomcat = new Tomcat();
 
             int webPort = config.getHttpPort();
             if (webPort <= 0) throw new Exception("httpPort configured to an invalid value of " + webPort);
 
-            Connector ctr = new Connector();
-            ctr.setPort(webPort);
-            tomcat.setConnector(ctr);
-
-            LOGGER.info("Configuring app with basedir: " + webappDirLocation.getAbsolutePath());
-
-            String contextPath = "";
-            StandardContext ctx = (StandardContext) tomcat.addContext(contextPath, webappDirLocation.getAbsolutePath());
-            ctx.addWelcomeFile("index.html");
-
-            tomcat.addServlet(contextPath, "api", new Api());
-            ctx.addServletMappingDecoded("/api", "api");
-
-            tomcat.addServlet(contextPath, "default", new DefaultServlet());
-            ctx.addServletMappingDecoded("/", "default");
-
-            ctx.addMimeMapping("svg", "image/svg+xml");
-
-            ctx.addApplicationLifecycleListener(new Listener());
+            Tomcat tomcat = buildTomcat(ConfigReader.getWebappDir(), ConfigReader.getUiDir(), webPort);
 
             tomcat.start();
             tomcat.getServer().await();
         } else {
             if (!ConfigReader.exists() || has(args,"--force-install")) {
-                new Installer(has(args,"--override-config-modifications")).run();
+                new Installer(has(args,"--override-config-files")).run();
             } else {
                 System.out.println("Conkw installation looks complete. Here are the flags you can use:");
                 System.out.println("");
-                System.out.println("  --force-install                 : Forces installation to happen again.");
-                System.out.println("  --override-config-modifications : Combined with the above flag, resets the");
-                System.out.println("                                    configuration.");
+                System.out.println("  --force-install         : Forces installation to happen again.");
+                System.out.println("  --override-config-files : Combined with the above flag, resets the");
+                System.out.println("                            configuration.");
                 System.out.println("");
                 System.out.println("In both cases, if --force-install the current config file will be kept, either");
-                System.out.println("as the actual config, or as the backup config if --override-config-modifications");
+                System.out.println("as the actual config, or as the backup config if --override-config-files");
                 System.out.println("is used.");
             }
         }
+    }
+
+    private static Tomcat buildTomcat(File webappDir, File uiDir, int webPort) {
+        Tomcat tomcat = new Tomcat();
+
+        Connector ctr = new Connector();
+        ctr.setPort(webPort);
+        tomcat.setConnector(ctr);
+
+        LOGGER.info("Configuring app with basedir: " + webappDir.getAbsolutePath());
+
+        addMainContext(webappDir, tomcat);
+        addUiContext(uiDir, tomcat);
+        return tomcat;
+    }
+
+    private static void addUiContext(File uiDir, Tomcat tomcat) {
+        String contextPath = "/ui";
+        StandardContext ctx = (StandardContext) tomcat.addContext(contextPath, uiDir.getAbsolutePath());
+        ctx.addWelcomeFile("index.html");
+        tomcat.addServlet(contextPath, "default", new DefaultServlet());
+        ctx.addServletMappingDecoded("/", "default");
+        ctx.addMimeMapping("svg", "image/svg+xml");
+    }
+
+    private static void addMainContext(File webappDirLocation, Tomcat tomcat) {
+        String contextPath = "";
+        StandardContext ctx = (StandardContext) tomcat.addContext(contextPath, webappDirLocation.getAbsolutePath());
+        ctx.addWelcomeFile("index.html");
+
+        tomcat.addServlet(contextPath, "api", new Api());
+        ctx.addServletMappingDecoded("/api", "api");
+
+        tomcat.addServlet(contextPath, "default", new DefaultServlet());
+        ctx.addServletMappingDecoded("/", "default");
+
+        ctx.addMimeMapping("svg", "image/svg+xml");
+
+        ctx.addApplicationLifecycleListener(new Listener());
     }
 
     private static boolean has(String[] args, String lookFor) {
