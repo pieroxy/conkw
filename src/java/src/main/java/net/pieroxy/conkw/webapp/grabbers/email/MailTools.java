c07856272db74273b579ce72df0adc4f1482ab78
Pierre Grimaud <pieroxy@pieroxy.net>
2021-08-06 07:32:12 +0200

    ✨ More mail tools

--- src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/email/MailTools.java
+++ src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/email/MailTools.java
@@ -1,9 +1,14 @@
 package net.pieroxy.conkw.webapp.grabbers.email;
 
+import com.sun.mail.imap.IMAPFolder;
+import org.apache.commons.mail.util.MimeMessageParser;
+
 import javax.mail.Address;
+import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class MailTools {
     public static String getFrom(Message message) throws MessagingException {
@@ -23,5 +28,32 @@ public class MailTools {
         } else {
             return address.toString();
         }
+    }
+
+    public static String getMailAddress(Address address) throws MessagingException {
+        if (address instanceof InternetAddress) {
+            InternetAddress ia = (InternetAddress) address;
+            return  ia.getAddress();
+        } else {
+            return address.toString();
+        }
+    }
+
+    public static Long getNextUid(Message m) throws MessagingException {
+        Folder f = m.getFolder();
+        if (f instanceof IMAPFolder) {
+            return ((IMAPFolder) f).getUIDNext();
+        }
+        return null;
+    }
+
+    public static String getPlainContent(MimeMessage message) throws Exception {
+        return new MimeMessageParser(message).parse().getPlainContent();
+    }
+
+    public static CharSequence getPlainContent(Object content) throws Exception {
+        if (content instanceof String) return (String)content;
+        if (content instanceof MimeMessage) return getPlainContent((MimeMessage) content);
+        return String.valueOf(content);
     }
 }
