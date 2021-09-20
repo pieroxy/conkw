32a88e9e2c07c2d6d609f19edeed633c6e675262
Pierre Grimaud <pieroxy@pieroxy.net>
2021-09-20 08:13:40 +0200

    ✨ Reset collector before grabbing.

--- src/java/src/main/java/net/pieroxy/conkw/grabbersBase/AsyncGrabber.java
+++ src/java/src/main/java/net/pieroxy/conkw/grabbersBase/AsyncGrabber.java
@@ -102,6 +102,7 @@ public abstract class AsyncGrabber extends SimpleGrabber<SimpleCollector> implem
             try {
               getActiveCollectors().forEach(sc -> {
                 long a = System.nanoTime();
+                sc.reset();
                 this.grabSync(sc);
                 sc.setTime(System.nanoTime() - a);
               });
