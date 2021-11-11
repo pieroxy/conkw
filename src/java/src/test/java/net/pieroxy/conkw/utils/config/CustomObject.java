9f3582e6a1a1e7d1843116da827eecb5a1ba6f1d
Pierre Grimaud <pieroxy@pieroxy.net>
2021-11-11 21:23:08 +0100

    ✨ GrabberConfigReader implementation for custom objects standalone and in lists.

--- src/java/src/test/java/net/pieroxy/conkw/utils/config/CustomObject.java
+++ src/java/src/test/java/net/pieroxy/conkw/utils/config/CustomObject.java
@@ -1,2 +1,42 @@
-package net.pieroxy.conkw.utils.config;public class CustomObject {
+package net.pieroxy.conkw.utils.config;
+
+import java.util.List;
+
+public class CustomObject {
+  private Double doubleValue;
+  private List<Double> doubleList;
+  private CustomObject subObject;
+  private List<CustomObject> customList;
+
+  public Double getDoubleValue() {
+    return doubleValue;
+  }
+
+  public void setDoubleValue(Double doubleValue) {
+    this.doubleValue = doubleValue;
+  }
+
+  public List<Double> getDoubleList() {
+    return doubleList;
+  }
+
+  public void setDoubleList(List<Double> doubleList) {
+    this.doubleList = doubleList;
+  }
+
+  public CustomObject getSubObject() {
+    return subObject;
+  }
+
+  public void setSubObject(CustomObject subObject) {
+    this.subObject = subObject;
+  }
+
+  public List<CustomObject> getCustomList() {
+    return customList;
+  }
+
+  public void setCustomList(List<CustomObject> customList) {
+    this.customList = customList;
+  }
 }
---
+++ src/java/src/test/java/net/pieroxy/conkw/utils/config/ObjectWithCustomField.java
@@ -0,0 +1,13 @@
+package net.pieroxy.conkw.utils.config;
+
+public class ObjectWithCustomField {
+  private CustomObject object;
+
+  public CustomObject getObject() {
+    return object;
+  }
+
+  public void setObject(CustomObject object) {
+    this.object = object;
+  }
+}
