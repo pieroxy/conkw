5564e0a872cea06615304902d545c8ac7bb341a2
Pierre Grimaud <pieroxy@pieroxy.net>
2021-11-11 09:10:40 +0100

    🚨 Small factorization of tests

--- src/java/src/test/java/net/pieroxy/conkw/utils/config/GrabberConfigReaderTest.java
+++ src/java/src/test/java/net/pieroxy/conkw/utils/config/GrabberConfigReaderTest.java
@@ -9,94 +9,87 @@ import java.nio.charset.StandardCharsets;
 
 public class GrabberConfigReaderTest extends TestCase {
     private final String TEST_SIMPLE = "{\"version\":1, \"object\":{\"boolValue\":true,\"doubleValue\":123,\"stringValue\":\"tsv\"}}";
-    private final String TEST_NULLS = "{\"version\":1, \"object\":{\"boolValue\":null,\"doubleValue\":null,\"stringValue\":null}}";
-    private final String TEST_MISSING = "{\"version\":1, \"object\":{}}";
-    private final String TEST_SIMPLE_LISTS = "{\"version\":2, \"object\":{" +
+    private final String TEST_NULLS = "{\"version\":2, \"object\":{\"boolValue\":null,\"doubleValue\":null,\"stringValue\":null}}";
+    private final String TEST_MISSING = "{\"version\":3, \"object\":{}}";
+    private final String TEST_SIMPLE_LISTS = "{\"version\":4, \"object\":{" +
             "\"boolValues\":[true,false,false,true,false,true]," +
             "\"doubleValues\":[123,2,6,4,2.5e12,-5]," +
             "\"stringValues\":[\"tsv\",\"tsv2\",\"\"]}}";
 
     public void testSimpleTypes() {
-        try {
-            TestModel data = JsonHelper.getJson().deserialize(TestModel.class, new ByteArrayInputStream(TEST_SIMPLE.getBytes(StandardCharsets.UTF_8)));
-            assertEquals(1, data.version);
+        TestModel data = parseJson(TEST_SIMPLE);
+        assertEquals(1, data.version);
 
-            SimpleObjectWithFields o = new SimpleObjectWithFields();
-            GrabberConfigReader.fillObject(o, data.object);
-            assertNotNull(o.getBoolValue());
-            assertNotNull(o.getStringValue());
-            assertNotNull(o.getDoubleValue());
-            assertEquals("tsv", o.getStringValue());
-            assertEquals(123., o.getDoubleValue());
-            assertEquals(Boolean.TRUE, o.getBoolValue());
-        } catch (IOException e) {
-            fail("Threw an exception : " + e.getMessage());
-        }
+        SimpleObjectWithFields o = new SimpleObjectWithFields();
+        GrabberConfigReader.fillObject(o, data.object);
+        assertNotNull(o.getBoolValue());
+        assertNotNull(o.getStringValue());
+        assertNotNull(o.getDoubleValue());
+        assertEquals("tsv", o.getStringValue());
+        assertEquals(123., o.getDoubleValue());
+        assertEquals(Boolean.TRUE, o.getBoolValue());
     }
 
     public void testSimpleNulls() {
-        try {
-            TestModel data = JsonHelper.getJson().deserialize(TestModel.class, new ByteArrayInputStream(TEST_NULLS.getBytes(StandardCharsets.UTF_8)));
-            assertEquals(1, data.version);
+        TestModel data = parseJson(TEST_NULLS);
+        assertEquals(2, data.version);
 
-            SimpleObjectWithFields o = new SimpleObjectWithFields();
-            GrabberConfigReader.fillObject(o, data.object);
-            assertNull(o.getBoolValue());
-            assertNull(o.getStringValue());
-            assertNull(o.getDoubleValue());
-        } catch (IOException e) {
-            fail("Threw an exception : " + e.getMessage());
-        }
+        SimpleObjectWithFields o = new SimpleObjectWithFields();
+        GrabberConfigReader.fillObject(o, data.object);
+        assertNull(o.getBoolValue());
+        assertNull(o.getStringValue());
+        assertNull(o.getDoubleValue());
     }
 
     public void testMissing() {
-        try {
-            TestModel data = JsonHelper.getJson().deserialize(TestModel.class, new ByteArrayInputStream(TEST_MISSING.getBytes(StandardCharsets.UTF_8)));
-            assertEquals(1, data.version);
+        TestModel data = parseJson(TEST_MISSING);
+        assertEquals(3, data.version);
 
-            SimpleObjectWithFields o = new SimpleObjectWithFields();
-            GrabberConfigReader.fillObject(o, data.object);
-            assertNull(o.getBoolValue());
-            assertNull(o.getStringValue());
-            assertNull(o.getDoubleValue());
-        } catch (IOException e) {
-            fail("Threw an exception : " + e.getMessage());
-        }
+        SimpleObjectWithFields o = new SimpleObjectWithFields();
+        GrabberConfigReader.fillObject(o, data.object);
+        assertNull(o.getBoolValue());
+        assertNull(o.getStringValue());
+        assertNull(o.getDoubleValue());
     }
 
     public void testSimpleLists() {
-        try {
-            TestModel data = JsonHelper.getJson().deserialize(TestModel.class, new ByteArrayInputStream(TEST_SIMPLE_LISTS.getBytes(StandardCharsets.UTF_8)));
-            assertEquals(2, data.version);
+        TestModel data = parseJson(TEST_SIMPLE_LISTS);
+        assertEquals(4, data.version);
 
-            SimpleObjectWithSimpleList o = new SimpleObjectWithSimpleList();
-            GrabberConfigReader.fillObject(o, data.object);
-            assertNotNull(o.getBoolValues());
-            assertNotNull(o.getStringValues());
-            assertNotNull(o.getDoubleValues());
-            assertEquals(3, o.getStringValues().size());
-            assertEquals(6, o.getDoubleValues().size());
-            assertEquals(6, o.getBoolValues().size());
-            int index = 0;
-            assertEquals("tsv", o.getStringValues().get(index++));
-            assertEquals("tsv2", o.getStringValues().get(index++));
-            assertEquals("", o.getStringValues().get(index++));
-            index = 0;
-            assertEquals(123., o.getDoubleValues().get(index++));
-            assertEquals(2., o.getDoubleValues().get(index++));
-            assertEquals(6., o.getDoubleValues().get(index++));
-            assertEquals(4., o.getDoubleValues().get(index++));
-            assertEquals(25e11, o.getDoubleValues().get(index++));
-            assertEquals(-5., o.getDoubleValues().get(index++));
-            index = 0;
-            assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
-            assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
-            assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
-            assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
-            assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
-            assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
+        SimpleObjectWithSimpleList o = new SimpleObjectWithSimpleList();
+        GrabberConfigReader.fillObject(o, data.object);
+        assertNotNull(o.getBoolValues());
+        assertNotNull(o.getStringValues());
+        assertNotNull(o.getDoubleValues());
+        assertEquals(3, o.getStringValues().size());
+        assertEquals(6, o.getDoubleValues().size());
+        assertEquals(6, o.getBoolValues().size());
+        int index = 0;
+        assertEquals("tsv", o.getStringValues().get(index++));
+        assertEquals("tsv2", o.getStringValues().get(index++));
+        assertEquals("", o.getStringValues().get(index++));
+        index = 0;
+        assertEquals(123., o.getDoubleValues().get(index++));
+        assertEquals(2., o.getDoubleValues().get(index++));
+        assertEquals(6., o.getDoubleValues().get(index++));
+        assertEquals(4., o.getDoubleValues().get(index++));
+        assertEquals(25e11, o.getDoubleValues().get(index++));
+        assertEquals(-5., o.getDoubleValues().get(index++));
+        index = 0;
+        assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
+        assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
+        assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
+        assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
+        assertEquals(Boolean.FALSE, o.getBoolValues().get(index++));
+        assertEquals(Boolean.TRUE, o.getBoolValues().get(index++));
+    }
+
+    TestModel parseJson(String json) {
+        try {
+            return JsonHelper.getJson().deserialize(TestModel.class, new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)));
         } catch (IOException e) {
-            fail("Threw an exception : " + e.getMessage());
+            throw new RuntimeException(e);
         }
+
     }
 }
