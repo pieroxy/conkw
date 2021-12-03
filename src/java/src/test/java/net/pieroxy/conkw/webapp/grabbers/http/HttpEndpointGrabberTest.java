a4ae1c2ceb63c7ccf7397d4e6af64e97c47c91e5
Pierre Grimaud <pieroxy@pieroxy.net>
2021-12-03 08:33:01 +0100

    ✨ HttpEndpointGrabber: Added Google workspace, and now the grabber exposes the number of matches.

--- src/java/src/test/java/net/pieroxy/conkw/webapp/grabbers/http/HttpEndpointGrabberTest.java
+++ src/java/src/test/java/net/pieroxy/conkw/webapp/grabbers/http/HttpEndpointGrabberTest.java
@@ -6,6 +6,7 @@ import net.pieroxy.conkw.collectors.SimpleTransientCollector;
 import net.pieroxy.conkw.webapp.model.ResponseData;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HttpEndpointGrabberTest extends ConkwTestCase {
@@ -30,18 +31,34 @@ public class HttpEndpointGrabberTest extends ConkwTestCase {
         conf.getToExtract().get(2).setId("pat3");
         conf.getToExtract().get(2).setNumber(false);
         conf.getToExtract().get(2).setPattern(Pattern.compile(".*data(.*)toto"));
+        conf.getToExtract().add(new HttpEndpointGrabber.EndPointMonitoringPatternConfig());
+        conf.getToExtract().get(3).setId("pat4");
+        conf.getToExtract().get(3).setNumber(true);
+        conf.getToExtract().get(3).setPattern(Pattern.compile("\\d+"));
 
-        grabber.collectDataFromHttp(0, 100, 200, "lots of data123", conf, sc);
+        grabber.collectDataFromHttp(0, 100, 200, "lots (2) of data123", conf, sc);
         sc.collectionDone();
 
         ResponseData data = sc.getDataCopy();
         assertMapContains(data.getNum(), "ex.firstByte.time", 100.);
         assertMapContains(data.getNum(), "ex.lastByte.time", 200.);
+
         assertMapContains(data.getNum(), "ex.pat1.matched", 1.);
-        assertMapContains(data.getNum(), "ex.pat2.matched", 1.);
-        assertMapContains(data.getNum(), "ex.pat3.matched", 0.);
         assertMapContains(data.getStr(), "ex.pat1.captured", "of");
+        assertMapContains(data.getNum(), "ex.pat1.found", 0.);
+
         assertMapContains(data.getNum(), "ex.pat2.captured", 123.);
+        assertMapContains(data.getNum(), "ex.pat2.matched", 1.);
+        assertMapContains(data.getNum(), "ex.pat2.found", 0.);
+
+        assertMapContains(data.getNum(), "ex.pat3.matched", 0.);
+        assertMapContains(data.getNum(), "ex.pat3.found", 0.);
         assertMapDoesNotContain(data.getNum(), "ex.pat3.captured");
+        assertMapDoesNotContain(data.getStr(), "ex.pat3.captured");
+
+        assertMapContains(data.getNum(), "ex.pat4.matched", 0.);
+        assertMapContains(data.getNum(), "ex.pat4.found", 2.);
+        assertMapDoesNotContain(data.getNum(), "ex.pat4.captured");
+        assertMapDoesNotContain(data.getStr(), "ex.pat4.captured");
     }
 }
