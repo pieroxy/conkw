0235ecabc1c35b6f16c52ecb36c153b466d94e40
Pierre Grimaud <pieroxy@pieroxy.net>
2021-12-08 08:38:10 +0100

    ✨ CDuration: Added one more static value, ordered them by increasing value.

--- src/java/src/main/java/net/pieroxy/conkw/utils/duration/CDuration.java
+++ src/java/src/main/java/net/pieroxy/conkw/utils/duration/CDuration.java
@@ -3,9 +3,10 @@ package net.pieroxy.conkw.utils.duration;
 public final class CDuration {
   private final long seconds;
   public static final CDuration ZERO = CDurationParser.parse("0m");
-  public static final CDuration ONE_MINUTE = CDurationParser.parse("1m");
-  public static final CDuration ONE_HOUR = CDurationParser.parse("1h");
   public static final CDuration FIVE_SECONDS = CDurationParser.parse("5s");
+  public static final CDuration ONE_MINUTE = CDurationParser.parse("1m");
+  public static final CDuration FIVE_MINUTES = CDurationParser.parse("5m");
+  public static final CDuration ONE_HOUR = CDurationParser.parse("1h");
 
   public CDuration(long numberOfSeconds) {
     this.seconds = numberOfSeconds;
