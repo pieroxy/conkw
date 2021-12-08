4e3be12347328d4a43741fc125037d30248147d1
Pierre Grimaud <pieroxy@pieroxy.net>
2021-12-08 09:36:30 +0100

    ✨ LatestUnreadMailsGrabber migrated to new config.

--- doc/GRABBER_LATEST_EMAILS.md
+++ doc/GRABBER_LATEST_EMAILS.md
@@ -13,40 +13,41 @@ This is the latest unread emails grabber. It fetches your unread emails from one
 
 ```json
 {
-    "implementation":"net.pieroxy.conkw.webapp.grabbers.email.LatestUnreadMailsGrabber",
-    "parameters": {
-        "ttl":"5m",
-        "maxMessages":"10"
+  "implementation":"net.pieroxy.conkw.webapp.grabbers.email.LatestUnreadMailsGrabber",
+  "name":"mails",
+  "config": {
+    "ttl":"5m",
+    "maxMessages":10,
+    "accounts": [
+      {
+        "name": "my gmail account",
+        "server":"imap.googlemail.com",
+        "port":993,
+        "login":"my_account@gmail.com",
+        "password":"myPassword"
       },
-      "namedParameters": {
-        "my gmail account": {
-          "server":"imap.googlemail.com",
-          "port":"993",
-          "login":"my_account@gmail.com",
-          "password":"mypassword"
-        },
-        "hotmail !!": {
-          "server":"outlook.office365.com",
-          "port":"993",
-          "login":"my_email@hotmail.com",
-          "password":"mypassword"
-        }
-    }
+      {
+        "name": "my hotmail account",
+        "server":"outlook.office365.com",
+        "port":993,
+        "login":"my_email@hotmail.com",
+        "password":"Do not use this as a password"
+      }
+    ]
+  }
 }
 ```
 
 * As this is a `TimeThrottledGrabber`, you can define `ttl` and `errorTtl`. [See here for more details](CONFIGURE.md). The default ttl is 5 minutes.
-* `maxMessages` is the maximum number of messages you want to return for the UI.
-* `imapConfN` is the list of email configurations.
-    * `N` starts at zero.
-    * This grabber will increase `N` by one until it cannot find a `imapConfN` anymore.
+* `maxMessages` is the maximum number of messages you want to return for the UI. Default value is 10.
+* `accounts` is the list of email accounts configurations.
     * Mails wil be mixed in the output, ordered by their receive date.
-    * Each configuration is on the form of `name:server:port:email:password`
-    * `name` is the name used for this configuration. Used for the UI to differentiate sources.
-    * `server` is the server. Above you can find examples for outlook and gmail.
-    * `port` is the port, usually 993.
-    * `email` is your login on the IMAP server. It usually your email address.
-    * `mypassword` is the password for the account. For Outlook accounts, it is the password of your account. For gmail accounts, you must [setup an application password](https://support.google.com/accounts/answer/185833) for ConkW and put it here. Note that `:` is allowed in a password in this string.
+    * Each configuration is :
+      * `name` is the name used for this configuration. Used for the UI to differentiate sources.
+      * `server` is the server. Above you can find examples for outlook and gmail.
+      * `port` is the port, usually 993.
+      * `email` is your login on the IMAP server. It usually your email address.
+      * `mypassword` is the password for the account. For Outlook accounts, it is the password of your account. For gmail accounts, you must [setup an application password](https://support.google.com/accounts/answer/185833) for ConkW and put it here.
 
 ## Metrics
 
@@ -62,3 +63,4 @@ For each email returned, the following metrics are exposed (Here for the 1st ema
 * `str.mail_0_account` The account this email came from. This is the name on the `imapConf` property. See the "Configuration" section above for more details.
 * `str.mail_0_from` The email of the sender, and name if available, in the form `Name <email>`.
 * `str.mail_0_subject` The subject of the email.
+* `str.mail_0_name` The name of the account.
--- src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/email/LatestUnreadMailsGrabber.java
+++ src/java/src/main/java/net/pieroxy/conkw/webapp/grabbers/email/LatestUnreadMailsGrabber.java
@@ -1,42 +1,49 @@
 package net.pieroxy.conkw.webapp.grabbers.email;
 
 import net.pieroxy.conkw.collectors.SimpleCollector;
-import net.pieroxy.conkw.utils.hashing.HashTools;
+import net.pieroxy.conkw.utils.StringUtil;
 import net.pieroxy.conkw.utils.duration.CDuration;
-import net.pieroxy.conkw.utils.duration.CDurationParser;
 import net.pieroxy.conkw.grabbersBase.TimeThrottledGrabber;
+import net.pieroxy.conkw.utils.hashing.Md5Sum;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.search.FlagTerm;
 import java.util.*;
 import java.util.logging.Level;
-import java.util.stream.Collectors;
 
 public class LatestUnreadMailsGrabber extends TimeThrottledGrabber<LatestUnreadMailsGrabber.LatestUnreadMailsGrabberConfig> {
     static final String NAME = "mails";
 
-    private int maxMessages = 10;
-    private String cackeKey = "nothing";
-    private List<ImapConfig> configurations = new ArrayList<>();
-
     private static Message[] getMessages(ImapConfig conf, boolean debug) throws MessagingException {
-        Session session = Session.getDefaultInstance(new Properties());
-        session.setDebug(debug);
-        Store store = session.getStore("imaps");
-        store.connect(conf.getServer(), conf.getPort(), conf.getLogin(), conf.getPassword());
-        Folder inbox = store.getFolder( "INBOX" );
-        inbox.open( Folder.READ_ONLY );
+        if (isValidPassword(conf.getPassword())) {
+            Session session = Session.getDefaultInstance(new Properties());
+            session.setDebug(debug);
+            Store store = session.getStore("imaps");
+            store.connect(conf.getServer(), (int) (double) conf.getPort(), conf.getLogin(), conf.getPassword());
+            Folder inbox = store.getFolder("INBOX");
+            inbox.open(Folder.READ_ONLY);
 
-        // Fetch unseen messages from inbox folder
-        Message[] messages = inbox.search(
-                new FlagTerm(new Flags(Flags.Flag.SEEN), false));
-        return messages;
+            // Fetch unseen messages from inbox folder
+            Message[] messages = inbox.search(
+                    new FlagTerm(new Flags(Flags.Flag.SEEN), false));
+            return messages;
+        } else {
+            return new Message[0];
+        }
     }
 
+    private static boolean isValidPassword(String password) {
+        return !StringUtil.isNullOrEmptyTrimmed(password);
+    }
+
+    @Override
+    public LatestUnreadMailsGrabberConfig getDefaultConfig() {
+        LatestUnreadMailsGrabberConfig c = new LatestUnreadMailsGrabberConfig();
+        c.setTtl(CDuration.FIVE_MINUTES);
+        c.setMaxMessages(10.);
+        return c;
+    }
 
     @Override
     public String getDefaultName() {
@@ -44,37 +51,11 @@ public class LatestUnreadMailsGrabber extends TimeThrottledGrabber<LatestUnreadM
     }
 
     @Override
-    protected void applyConfig(Map<String, String> config, Map<String, Map<String, String>> configs) {
-        maxMessages = getIntProperty("maxMessages", config, maxMessages);
-        configurations = new ArrayList<>();
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            log(Level.SEVERE, "", e);
-            return;
-        }
-        md.update(String.valueOf(maxMessages).getBytes(StandardCharsets.UTF_8));
-
-        for (String name : configs.keySet().stream().sorted().collect(Collectors.toList())) {
-            ImapConfig ic = new ImapConfig(name, configs.get(name));
-            configurations.add(ic);
-            md.update(ic.getUniqueContentString().getBytes(StandardCharsets.UTF_8));
-        }
-        cackeKey = HashTools.byteArrayToHexString(md.digest());
-    }
-
-    @Override
-    protected CDuration getDefaultTtl() {
-        return CDurationParser.parse("5m");
-    }
-
-    @Override
     protected void load(SimpleCollector res) {
         long now = System.currentTimeMillis();
         log(Level.FINE, "Starting to grab mails");
         List<MailData> allMails = new ArrayList<>();
-        for (ImapConfig c : configurations) {
+        for (ImapConfig c : getConfig().getAccounts()) {
             try {
                 Message[]ms = getMessages(c, this.canLogFiner());
                 if (shutdownRequested()) return;
@@ -97,19 +78,43 @@ public class LatestUnreadMailsGrabber extends TimeThrottledGrabber<LatestUnreadM
             res.collect("mail_" + i + "_account", m.getAccount());
             res.collect("mail_" + i + "_from", m.getFrom());
             i++;
-            if (i>=maxMessages) break;
+            if (i>=getConfig().getMaxMessages()) break;
         }
         res.collect("mails_len", i);
         res.collect("mails_unread_total", allMails.size());
         log(Level.FINE, "Done grabbing mails in " + (System.currentTimeMillis() - now) + "ms.");
     }
 
-    @Override
-    protected String getCacheKey() {
-        return cackeKey;
-    }
-
     public static class LatestUnreadMailsGrabberConfig extends TimeThrottledGrabber.TimeThrottledGrabberConfig {
+        private Double maxMessages;
+        private List<ImapConfig> accounts;
 
+        @Override
+        public void addToHash(Md5Sum sum) {
+            sum.add(String.valueOf(maxMessages));
+            getAccounts().stream().forEach(ic -> {
+                sum.add(ic.getLogin());
+                sum.add(ic.getName());
+                sum.add(ic.getPassword());
+                sum.add(ic.getServer());
+                sum.add(String.valueOf(ic.getPort()));
+            });
+        }
+
+        public Double getMaxMessages() {
+            return maxMessages;
+        }
+
+        public void setMaxMessages(Double maxMessages) {
+            this.maxMessages = maxMessages;
+        }
+
+        public List<ImapConfig> getAccounts() {
+            return accounts;
+        }
+
+        public void setAccounts(List<ImapConfig> accounts) {
+            this.accounts = accounts;
+        }
     }
 }
