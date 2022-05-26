b424e555dba8070d0814567052d8c3adb59843f5
Pierre Grimaud <pieroxy@pieroxy.net>
2022-05-26 10:17:37 +0200

    ♻️🚨 Migrated SimpleCounterTest to AbstractAccumulatorTest

--- src/java/src/test/java/net/pieroxy/conkw/accumulators/implementations/SimpleCounterTest.java
+++ src/java/src/test/java/net/pieroxy/conkw/accumulators/implementations/SimpleCounterTest.java
@@ -1,9 +1,12 @@
 package net.pieroxy.conkw.accumulators.implementations;
 
-import net.pieroxy.conkw.ConkwTestCase;
 import net.pieroxy.conkw.utils.PrefixedKeyMap;
 
-public class SimpleCounterTest extends ConkwTestCase {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SimpleCounterTest extends AbstractAccumulatorTest<SimpleCounter<Data>> {
   public void testSimple() {
     SimpleCounter<Data> sa = new SimpleCounter<>();
     sa.add(new Data().addVal("vvv", 12.));
@@ -12,19 +15,31 @@ public class SimpleCounterTest extends ConkwTestCase {
     sa.prepareNewSession();
     assertEquals(3., sa.getTotal());
   }
-  public void testInitializeFromData() {
-    SimpleCounter<Data> sa = new SimpleCounter<>();
-    for (int i=0 ; i<123 ; i++) sa.add(null);
-    sa.prepareNewSession();
 
-    Data log = new Data();
-    sa.log("", log.getValues(), log.getDimensions());
-    SimpleCounter sa2 = new SimpleCounter();
-    sa2.initializeFromData(new PrefixedKeyMap<>(log.getValues()), new PrefixedKeyMap<>(log.getDimensions()));
-    sa2.prepareNewSession();
-    assertEquals(123., sa2.getTotal());
-    Data log2 = new Data();
-    sa2.log("", log2.getValues(), log2.getDimensions());
-    log.assertEquals(log2, this);
+  @Override
+  protected SimpleCounter<Data> buildAccumulator() {
+    return new SimpleCounter<>();
+  }
+
+  @Override
+  protected Collection<Data> buildData() {
+    List<Data> ld = new ArrayList<>();
+    for (int i=0 ; i<123 ; i++) ld.add(null);
+    return ld;
+  }
+
+  @Override
+  protected void assertAccumulatorInternalState(SimpleCounter<Data> acc) {
+    assertEquals(123., acc.getTotal());
+  }
+
+  @Override
+  protected void assertAccumulatorLog(Data log) {
+    assertMapContains(log.getValues(), "count", 123.);
+  }
+
+  @Override
+  public void test() {
+
   }
 }
