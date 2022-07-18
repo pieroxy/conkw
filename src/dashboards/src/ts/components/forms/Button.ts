5c2f192eeba941183fec41bab300465404c3f503
Pierre Grimaud <pieroxy@pieroxy.net>
2022-07-18 23:12:36 +0200

    ✨ [DSHB][UI] Button styling for hover, active and focus states

--- src/dashboards/src/ts/components/forms/Button.ts
+++ src/dashboards/src/ts/components/forms/Button.ts
@@ -17,7 +17,7 @@ export class Button implements m.ClassComponent<ButtonInputAttrs> {
           attrs.action();
         },
         
-      }, children);
+      }, m(".innerbutton", children));
   }
 }
 
