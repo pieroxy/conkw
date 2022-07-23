c867552b0370667ba24c71051985450c3929f2ea
Pierre Grimaud <pieroxy@pieroxy.net>
2022-07-23 22:53:39 +0200

    ✨ [DSHB][UI] First choice of panel

--- src/dashboards/src/ts/components/pages/dashboards/NewPanel.ts
+++ src/dashboards/src/ts/components/pages/dashboards/NewPanel.ts
@@ -22,6 +22,35 @@ export class NewPanelPage extends AbstractPage<NewPanelPageAttrs> {
         m(RightChevronIcon),
         "Panel"
       ]),
+      m(".panel.clickable", [
+        m(".title", "Simple gauge with value and label"),
+        m(".line", [
+          m(".placeholderText", "Some thing"),
+          m(".placeholderGauge", {style:{borderLeftWidth:"5px"}}),
+          m(".placeholderValue", "5.00"),
+        ]),
+        m(".line", [
+          m(".placeholderText", "some other thing"),
+          m(".placeholderGauge", {style:{borderLeftWidth:"100px"}}),
+          m(".placeholderValue", "100."),
+        ]),
+        m(".line", [
+          m(".placeholderText", "The third one"),
+          m(".placeholderGauge", {style:{borderLeftWidth:"85px"}}),
+          m(".placeholderValue", "85.0"),
+        ]),
+        m(".line", [
+          m(".placeholderText", "All is lost"),
+          m(".placeholderGauge", {style:{borderLeftWidth:"135px"}}),
+          m(".placeholderValue", "135."),
+        ]),
+        m(".line", [
+          m(".placeholderText", "Or not"),
+          m(".placeholderGauge", {style:{borderLeftWidth:"52px"}}),
+          m(".placeholderValue", "52.0"),
+        ]),
+        m("br", {clear:"both"})
+      ])
     ]);
   }
 }
