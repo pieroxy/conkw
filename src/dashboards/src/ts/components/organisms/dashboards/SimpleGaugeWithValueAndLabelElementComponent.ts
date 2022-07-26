4440086eafeb1ef4926baf6aa78568a284d0d8a2
Pierre Grimaud <pieroxy@pieroxy.net>
2022-07-26 08:05:55 +0200

    ✨ [DSHB][UI] SimpleGaugeWithValueAndLabelElementComponent now looks like something. Not usable still but less ugly.

--- src/dashboards/src/ts/components/organisms/dashboards/SimpleGaugeWithValueAndLabelElementComponent.ts
+++ src/dashboards/src/ts/components/organisms/dashboards/SimpleGaugeWithValueAndLabelElementComponent.ts
@@ -1,5 +1,6 @@
 import m from 'mithril';
 import { SimpleGaugeWithValueAndLabelElement } from '../../../auto/pieroxy-conkw';
+import { DisplayUtils } from '../../../utils/DisplayUtils';
 import { SimpleGaugeComponent } from './atoms/SimpleGauge';
 import { TextLabelComponent } from './atoms/TextLabel';
 import { ValueWithUnitComponent } from './atoms/ValueWithUnit';
@@ -8,10 +9,11 @@ import { DashboardElement, DashboardElementAttrs } from './DashboardElement';
 export class SimpleGaugeWithValueAndLabelElementComponent 
 extends DashboardElement<SimpleGaugeWithValueAndLabelElementAttrs> {
   public view({attrs}:m.Vnode<SimpleGaugeWithValueAndLabelElementAttrs>):m.Children {
+    console.log("calc(100%-"+this.getRemainingWidth(attrs.model)+"px)");
     return m(".simpleGaugeValueLabel", [
-      m(".sgvl-label", m(TextLabelComponent, {
+      m(".sgvl-gauge", m(SimpleGaugeComponent, {
         currentData:attrs.currentData,
-        model:attrs.model.label,
+        model:attrs.model.gauge,
         parent:this,
       })),
       m(".sgvl-value", m(ValueWithUnitComponent, {
@@ -19,12 +21,19 @@ extends DashboardElement<SimpleGaugeWithValueAndLabelElementAttrs> {
         model:attrs.model.value,
         parent:this,
       })),
-      m(".sgvl-gauge", m(SimpleGaugeComponent, {
+      m(".sgvl-label", {
+        style: {
+          width:"calc(100% - "+this.getRemainingWidth(attrs.model)+"px - 1px)"
+        }
+      }, m(TextLabelComponent, {
         currentData:attrs.currentData,
-        model:attrs.model.gauge,
+        model:attrs.model.label,
         parent:this,
       })),
     ])
+  }
+  getRemainingWidth(model: SimpleGaugeWithValueAndLabelElement) {
+    return 155 + (5+model.value.unit.length)*DisplayUtils.getMonospaceCharWidth(14);
   }
 }
 
---
+++ src/dashboards/src/ts/utils/DisplayUtils.ts
@@ -0,0 +1,23 @@
+export class DisplayUtils {
+  private static monospaceCharWidth?:number = undefined;
+
+  public static getMonospaceCharWidth(fontSizeInPx:number):number {
+    if (!this.monospaceCharWidth) {
+      let div = document.createElement("DIV");
+      div.innerHTML = "a";
+      div.className = "monospace";
+      div.style.fontSize = "100px";
+      div.style.display = "inline-block";
+      div.style.margin = "0";
+      div.style.padding = "0";
+      document.body.appendChild(div);
+      this.monospaceCharWidth = div.clientWidth;
+      document.body.removeChild(div);
+      console.log(this.monospaceCharWidth);
+    }
+    let res = this.monospaceCharWidth / 100 * fontSizeInPx;
+    console.log("Width for " + fontSizeInPx + " is " + res);
+
+    return res;
+  }
+}
