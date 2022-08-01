c946f96e135856bf4ab418a319aabf63bd7bd767
Pierre Grimaud <pieroxy@pieroxy.net>
2022-08-01 09:13:01 +0200

    ✨ [DSHB][UI] Added credit for Tomcat

--- src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
+++ src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
@@ -8,7 +8,7 @@ export class LoginFooter implements m.ClassComponent {
       m("span", m("a", {href:"/doc/README.html", title:"Low level doc"}, "Documentation")),
       m("span", m("a", {href:"https://github.com/pieroxy/conkw", title:"The code, the community"}, "Github")),
       m("span", {title:AppVersion.GIT_REV + " - " + AppVersion.GIT_DEPTH}, "Ver: ", AppVersion.MVN_VER),
-      m("span", m("a", {href:"https://mithril.js.org/", title:"Do not settle for anything but the best"}, m("span", [m(MithrilIcon), " enabled by Mitril"])))
+      m("span", { title:"Do not settle for anything but the best"}, "Enabled by ", m("a", {href:"https://mithril.js.org/"}, m("span", [m(MithrilIcon), " Mitril"])), " and ", m("a", {href:"https://tomcat.apache.org/", title:"Do not settle for anything but the best"}, " Apache Tomcat"))
     ])
   }
 }
