264782e7e2a8f35610d18aaba9427bc2fee90211
Pierre Grimaud <pieroxy@pieroxy.net>
2022-08-01 08:57:32 +0200

    ✨ [DSHB][UI] Redid the footer, added credit to Mithril.

--- src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
+++ src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
@@ -1,13 +1,14 @@
 import m from 'mithril';
 import { AppVersion } from '../../../auto/version';
+import { MithrilIcon } from '../../icons/MithrilIcon';
 
 export class LoginFooter implements m.ClassComponent {
   view() {
     return m(".footer", [
-      m("span", m("a", {href:"/doc/README.html"}, "Documentation")),
-      m("span", m("a", {href:"https://github.com/pieroxy/conkw"}, "Github")),
-      m("span", "Ver: ", AppVersion.MVN_VER, " (", AppVersion.GIT_REV, ", " + AppVersion.GIT_DEPTH + ")")
+      m("span", m("a", {href:"/doc/README.html", title:"Low level doc"}, "Documentation")),
+      m("span", m("a", {href:"https://github.com/pieroxy/conkw", title:"The code, the community"}, "Github")),
+      m("span", {title:AppVersion.GIT_REV + " - " + AppVersion.GIT_DEPTH}, "Ver: ", AppVersion.MVN_VER),
+      m("span", m("a", {href:"https://mithril.js.org/", title:"Do not settle for anything but the best"}, m("span", [m(MithrilIcon), " enabled by Mitril"])))
     ])
-
   }
 }
