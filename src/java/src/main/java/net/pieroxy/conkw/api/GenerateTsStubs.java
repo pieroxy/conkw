1cd95c30973f215f2c3bc5cbf8dd4e5601b731e5
Pierre Grimaud <pieroxy@pieroxy.net>
2022-08-03 08:06:54 +0200

    🔧 Builds Api calls TS file in pom.xml for more convenience

---
+++ src/java/src/main/java/net/pieroxy/conkw/api/GenerateTsStubs.java
@@ -0,0 +1,94 @@
+package net.pieroxy.conkw.api;
+
+import net.pieroxy.conkw.api.metadata.AbstractApiEndpoint;
+import net.pieroxy.conkw.api.metadata.ApiEndpoint;
+import net.pieroxy.conkw.api.metadata.Endpoint;
+import net.pieroxy.conkw.utils.reflection.GetAccessibleClasses;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.util.*;
+
+public class GenerateTsStubs {
+    private static BufferedWriter writer;
+    private static List<String> later = new ArrayList<>();
+
+    private static void write(String s) throws IOException {
+        writer.write(s);
+        writer.newLine();
+    }
+
+    private static void writeLater(String s) {
+        later.add(s);
+    }
+
+    public static void main(String[]args) throws Exception {
+        File out = new File(args[0]);
+        writer = new BufferedWriter(new FileWriter(out));
+
+        write("/* tslint:disable */");
+        write("/* eslint-disable */");
+        write("// Generated on " + new Date());
+        write("import { Api } from \"../utils/api/Api\";");
+        writeLater("export class ApiEndpoints {");
+
+        Map<String, ApiEndpoint> newEp = new HashMap<>();
+        List<Class<?>> allclasses = GetAccessibleClasses.getClasses("net.pieroxy.conkw");
+        for (Class c : allclasses) {
+            if (ApiEndpoint.class.isAssignableFrom(c) && c.isAnnotationPresent(Endpoint.class)) {
+                Endpoint endpoint = (Endpoint)c.getAnnotation(Endpoint.class);
+                String input = getType(c,0);
+                String output = getType(c, 1);
+                String name = c.getSimpleName().substring(0, c.getSimpleName().length() - "Endpoint".length());
+
+                write("import { "+input+","+output+" } from \"./pieroxy-conkw\";");
+                writeLater("    public static "+name+" = {");
+                writeLater("        call:(input:"+input+"):Promise<"+output+"> => {");
+                writeLater("            return Api.call<"+input+", "+output+">({");
+                writeLater("                    method:\""+endpoint.method()+"\",");
+                writeLater("                    body:input,");
+                writeLater("                    endpoint:\""+name+"\"");
+                writeLater("            });");
+                writeLater("        }");
+                writeLater("    }");
+                writeLater("");
+            }
+        }
+
+        for (String s : later) write(s);
+        write("}");
+
+        writer.close();
+    }
+/*
+
+  public static DoChangePassword = {
+    method:"POST",
+    role:"ANONYMOUS",
+    call:(input:DoChangePasswordInput):Promise<DoChangePasswordOutput> => {
+      return Api.call<DoChangePasswordInput, DoChangePasswordOutput>({
+        method:"POST",
+        body:input,
+        endpoint:"DoChangePassword"
+      });
+    }
+  }
+
+ */
+
+
+
+
+    private static String getType(Class subClass, int i) {
+        while (subClass.getSuperclass() != AbstractApiEndpoint.class) {
+            // instance.getClass() is no subclass of classOfInterest or instance is a direct instance of classOfInterest
+            subClass = subClass.getSuperclass();
+            if (subClass == null) throw new IllegalArgumentException();
+        }
+        ParameterizedType parameterizedType = (ParameterizedType) subClass.getGenericSuperclass();
+        return ((Class)parameterizedType.getActualTypeArguments()[i]).getSimpleName();
+    }
+}
