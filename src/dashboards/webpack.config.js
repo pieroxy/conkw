e96b2bf09010eb5caee5110e01962b957a87f581
Pierre Grimaud <pieroxy@pieroxy.net>
2022-08-08 07:49:14 +0200

    👷 [DSHB] Use mini-css-extract-plugin for production mode && added sourcemaps for dev mode

--- src/dashboards/webpack.config.js
+++ src/dashboards/webpack.config.js
@@ -1,57 +1,81 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: './src/ts/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        include: path.resolve(__dirname, 'src/ts'),
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        include: path.resolve(__dirname, 'src/css'),
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-              url: {
-                filter: (url, resourcePath) => {
-                  // resourcePath - path to css file
-    
-                  // Don't handle `img.png` urls
-                  if (url.indexOf("/fonts/")==0) {
-                    return false;
-                  }
-                  return true;
-                },
+module.exports = (env, argv) => 
+{
+  const devMode = argv.mode !== "production";
+  return {
+    entry: './src/ts/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: path.resolve(__dirname, 'src/ts'),
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          include: path.resolve(__dirname, 'src/css'),
+          use: [
+            // Creates `style` nodes from JS strings
+            devMode ? "style-loader":
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it uses publicPath in webpackOptions.output
+                publicPath: "./",
               },
+            },
+            // Translates CSS into CommonJS
+            {
+              loader: 'css-loader',
+              options: {
+                url: {
+                  filter: (url, resourcePath) => {
+                    // resourcePath - path to css file
+      
+                    // Don't handle `img.png` urls
+                    if (url.indexOf("/fonts/")==0) {
+                      return false;
+                    }
+                    return true;
+                  },
+                },
+                sourceMap: true
+              }
+            },          
+            // Compiles Sass to CSS
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true
+              }
             }
-          },          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
+            ,
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle-[contenthash:6].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'ConkW Dashboards',
+        template: './src/html/index.html',
+  
+      }),
+      new MiniCssExtractPlugin({
+        filename:"bundle-[contenthash:6].css"
+      })
     ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle-[contenthash:6].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'ConkW Dashboards',
-      template: './src/html/index.html',
-
-    }),
-  ],
-};
+  }
+}
