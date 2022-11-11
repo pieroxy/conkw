288667f48ffbcdea7fc187f17362f704a16cfaeb
Pierre Grimaud <pieroxy@pieroxy.net>
2022-11-11 19:59:04 +0100

    ✨ [DSHB][UI] Small footer refacto, added tomcat icon

--- src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
+++ src/dashboards/src/ts/components/pages/auth/LoginFooter.ts
@@ -1,6 +1,7 @@
 import m from 'mithril';
 import { AppVersion } from '../../../auto/version';
 import { MithrilIcon } from '../../atoms/icons/MithrilIcon';
+import { TomcatIcon } from '../../atoms/icons/TomcatIcon';
 
 export class LoginFooter implements m.ClassComponent {
   view() {
@@ -8,7 +9,11 @@ export class LoginFooter implements m.ClassComponent {
       m("span", m("a", {href:"/doc/README.html", title:"Low level doc"}, "Documentation")),
       m("span", m("a", {href:"https://github.com/pieroxy/conkw", title:"The code, the community"}, "Github")),
       m("span", {title:AppVersion.GIT_REV + " - " + AppVersion.GIT_DEPTH}, "Ver: ", AppVersion.MVN_VER),
-      m("span", { title:"Do not settle for anything but the best"}, "Enabled by ", m("a", {href:"https://mithril.js.org/"}, m("span", [m(MithrilIcon), " Mitril"])), " and ", m("a", {href:"https://tomcat.apache.org/", title:"Do not settle for anything but the best"}, " Apache Tomcat"))
+      m("span", { title:"Do not settle for anything but the best"}, 
+          "Enabled by ", 
+          m("a", {href:"https://mithril.js.org/"}, [m(MithrilIcon), " Mitril"]), 
+          " and ", 
+          m("a", {href:"https://tomcat.apache.org/"}, m(TomcatIcon), " Apache Tomcat"))
     ])
   }
 }
