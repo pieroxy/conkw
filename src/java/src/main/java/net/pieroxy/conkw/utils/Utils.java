d820348db96099cf48555af5abcd5a8c410817fe
Pierre Grimaud <pieroxy@pieroxy.net>
2022-12-25 17:44:53 +0100

    🐛😱 The smallest things ...

--- src/java/src/main/java/net/pieroxy/conkw/utils/Utils.java
+++ src/java/src/main/java/net/pieroxy/conkw/utils/Utils.java
@@ -3,7 +3,7 @@ package net.pieroxy.conkw.utils;
 public class Utils {
     public static boolean objectEquals(Object a, Object b) {
         if (a==null && b==null) return true;
-        if (a==null || b==null) return true;
+        if (a==null || b==null) return false;
         return a.equals(b);
     }
 }
